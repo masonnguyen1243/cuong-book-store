86bff8d281a8f9cd1fcb8d0b2e5a452f28770b5f
cuongnguyenba1243 <2121050937@student.humg.edu.vn>
2025-05-14 13:35:39 +0700

    Setup frontend

---
+++ frontend/eslint.config.js
@@ -0,0 +1,42 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+
+export default [
+  { ignores: ["dist", "ignores"] },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "prettier/prettier": [
+        "warn",
+        {
+          arrowParens: "always",
+          tabWidth: 2,
+          endOfLine: "auto",
+          useTabs: false,
+          printWidth: 120,
+        },
+      ],
+      "no-console": "error",
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    },
+  },
+];
---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-green-100 underline">Hello world!</h1>
+    </div>
+  );
+};
+export default App;
