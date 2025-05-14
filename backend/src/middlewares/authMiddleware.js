48159ada121f1157d658d41b875648ea0de564d7
cuongnguyenba1243 <2121050937@student.humg.edu.vn>
2025-05-14 15:55:28 +0700

    Auth middlewares

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,41 @@
+import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
+import { ENV } from "../config/enviroment.js";
+
+export const verifyToken = async (req, res, next) => {
+  const accessToken = req.cookies?.accessToken;
+
+  if (!accessToken) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ success: false, message: "Token not found" });
+  }
+  try {
+    const decoded = jwt.verify(accessToken, ENV.JWT_ACCESS_TOKEN);
+
+    req.user = decoded;
+    console.log(req.user);
+
+    next();
+  } catch (error) {
+    if (error?.message?.includes("jwt expired")) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Token expired" });
+    }
+
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: error.message });
+  }
+};
+
+export const isAdmin = async (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ success: false, message: "REQUIRE ADMIN ROLE!" });
+  }
+};
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { authControllers } from "../controllers/authControllers.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
@@ -7,7 +8,7 @@ router.post("/register", authControllers.register);
 
 router.post("/login", authControllers.login);
 
-router.delete("/logout", authControllers.logout);
+router.delete("/logout", verifyToken, authControllers.logout);
 
 router.put("/verify-account", authControllers.verifyAccount);
 
